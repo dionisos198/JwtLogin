eb2de3fcb08be71bd22c3ac9dfca54a2ef80fb36
Jinu Yi <dionisos198@naver.com>
2023-09-11 02:43:39 +0900

    회원가입, 로그인 Dto

---
+++ src/main/java/com/login/jwt/dto/SignInDto.java
@@ -0,0 +1,14 @@
+package com.login.jwt.dto;
+
+import lombok.Getter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+@Getter
+public class SignInDto {
+    private String userID;
+    private String password;
+    public UsernamePasswordAuthenticationToken getAuthenticationToken(){
+        return new UsernamePasswordAuthenticationToken(userID,password);
+    }
+}
