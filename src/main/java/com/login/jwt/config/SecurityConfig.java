94a331f3219336979f7b29dd3dfa0982364e72c8
Jinu Yi <dionisos198@naver.com>
2023-09-11 02:47:46 +0900

    핵심 인증,인가 api 설정

---
+++ src/main/java/com/login/jwt/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.login.jwt.config;
+
+import com.login.jwt.config.jwt.JwtAccessDeniedHandler;
+import com.login.jwt.config.jwt.JwtAuthenticationEntryPointHandler;
+import com.login.jwt.config.jwt.JwtSecurityConfig;
+import com.login.jwt.config.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+   private final JwtAuthenticationEntryPointHandler authenticationEntryPointHandler;
+   private final JwtAccessDeniedHandler accessDeniedHandler;
+   private final TokenProvider tokenProvider;
+   private final RedisTemplate<String,String> redisTemplate;
+
+
+   @Bean
+   public BCryptPasswordEncoder passwordEncoder(){
+      return new BCryptPasswordEncoder();
+   }
+
+   @Bean
+   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+      return http.formLogin().disable()
+              .csrf().disable()
+              .sessionManagement()
+              .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+              .and()
+              .exceptionHandling()
+              .authenticationEntryPoint(authenticationEntryPointHandler)
+              .accessDeniedHandler(accessDeniedHandler)
+              .and()
+              .authorizeRequests()
+              .antMatchers("/auth/**").permitAll()
+              .antMatchers("/normal").access("hasRole('VIP') or hasRole('NORMAL')")
+              .antMatchers("/vip").hasRole("VIP")
+              .anyRequest().authenticated()
+              .and()
+
+              .apply(new JwtSecurityConfig(tokenProvider,redisTemplate))
+              .and().build();
+
+
+   }
+
+}
